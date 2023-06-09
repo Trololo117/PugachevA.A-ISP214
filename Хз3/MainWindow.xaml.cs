f72dc33b76d922d344916cf667cff8ad8982199a
Pugachev.A <134061774+Trololo117@users.noreply.github.com>
2023-06-09 20:28:51 +0400

    Хз3

---
+++ Хз3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickExit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void ClickChangeColor(object sender, RoutedEventArgs e)
+        {
+            window.Background = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+        }
+
+        private void ClickInfo(object sender, RoutedEventArgs e)
+        {
+            textblock.Text = "Я крутой";
+        }
+    }
+}
